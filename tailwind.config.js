64697cd3feca5cc62adea16f20490fdb9515bf37
Ludovic laurichesse <ludovic.laurichesse@outlook.fr>
2022-06-03 19:44:21 +0200

    :rocket:<init>all started here

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'primary': '#1EB8BE',
+      'secondary': '#F5DF99',
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#C3BBBB',
+      'gray-light': '#c5c5c5',
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+  },
+  plugins: [],
+}
